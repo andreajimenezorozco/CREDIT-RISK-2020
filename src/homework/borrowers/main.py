67f3c193d97fbf8baac2206a0d2bf977d001f415
Andrea Jimenez <andrea.jimenez@ieseg.fr>
2020-10-12 15:40:34 -0500

    Test 1 homework

--- src/homework/borrowers/main.py
+++ src/homework/borrowers/main.py
@@ -11,15 +11,17 @@ class Main:
     @staticmethod
     def show(file: str = DEFAULT_FILENAME) -> str:
         logger.info("Calling the show method.")
-        # TODO: read file and show content
-        pass
+        with open(file, "r") as f:
+            content = f.read()
+            return content
+
+    @staticmethod
+    def update(email: str, age: int, income: float, file: str = DEFAULT_FILENAME):
+        borrower = Borrower(email=email, age=age, income=income)
+        borrower.update(file=file)
 
     @staticmethod
     def insert(email: str, age: int, income: float, file: str = DEFAULT_FILENAME):
         borrower = Borrower(email=email, age=age, income=income)
         borrower.save(file=file)
 
-    @staticmethod
-    def update(email: str, age: int, income: float, file: str = DEFAULT_FILENAME):
-        borrower = Borrower(email=email, age=age, income=income)
-        borrower.update(file=file)
