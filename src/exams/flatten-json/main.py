f4f2b2a1b2dbe6ac907e815619403877f4c56e94
Andrea Jimenez <andrea.jimenez@ieseg.fr>
2020-10-16 17:52:59 -0500

    New version

--- src/exams/flatten-json/main.py
+++ src/exams/flatten-json/main.py
@@ -1,5 +1,18 @@
+import json
+from fintools.settings import get_logger
+
 def show(self, file):
     logger.info("Calling the show method.")
     with open(file, "r") as f:
         content = json.loads(f.read())
         return content
+
+def flatten(file):
+    with open(file, "r") as f:
+        info = json.loads(f.read())
+    return flatten_json(info)
+
+
+
+
+
