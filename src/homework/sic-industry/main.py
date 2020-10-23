2a32ed72e868a0d846906c8ec4286bec43ceef6a
Andrea Jimenez <andrea.jimenez@ieseg.fr>
2020-10-23 17:09:16 -0500

    i

--- src/homework/sic-industry/main.py
+++ src/homework/sic-industry/main.py
@@ -16,6 +16,27 @@ logger = get_logger(name=__name__)
 class Main:
     threshold = INDUSTRY_SEARCH_DEFAULT_THRESHOLD
 
-    @timeit(logger=logger)
-    def search(self, title: str, exact: bool = False, file: str = INDUSTRY_SEARCH_DEFAULT_FILENAME) -> str:
-        pass
+    def _recursive_search(self, node, string_wrapper, exact):
+        title = node["title"]
+        children = node["children"]
+        new_children = []
+
+        for child in children:
+            is_child_valid = self._recursive_search(child, string_wrapper, exact=exact)
+            if is_child_valid:
+                new_children.append(child)
+
+        node["children"] = new_children
+        search = len(new_children) or string_wrapper.boolean_search(title, reverse=True, exact=exact)
+        return search
+
+    @timeit(logger)
+    def search(self, title: str, exact: bool = False, filename: str = INDUSTRY_SEARCH_DEFAULT_FILENAME):
+        target_tittle = StringWrapper(value=title)
+        sic_industries = json.loads(filename)
+        children = sic_industries["children"]
+        new_children = []
+        for child in children:
+            if self._recursive_search(child, target_tittle, exact=exact):
+                new_children.append(child)
+        return new_children
