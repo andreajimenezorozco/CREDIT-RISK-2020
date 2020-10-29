c6ab3b59d0ba8e594a5184d720f0472de0efd23a
Andrea Jimenez <andrea.jimenez@ieseg.fr>
2020-10-28 18:37:12 -0600

    Homework

--- src/classwork/cashflow/main.py
+++ src/classwork/cashflow/main.py
@@ -5,10 +5,14 @@ from fintools import CashFlow
 
 class Main:
 
+    def __init__(self, amount: float, t: int):
+        self.amount = amount
+        self.t = t
+
     @staticmethod
     def present_value(amount: float, rate: float, n: int):
-        raise NotImplementedError
+        return CashFlow(amount=self.amount * (1+r) ** (-self.t), t=0)
 
     @staticmethod
     def future_value(amount: float, rate: float, n: int):
-        raise NotImplementedError
+        return CashFlow(amount=self.amount * (1 + r) ** (self.t), t=t)
