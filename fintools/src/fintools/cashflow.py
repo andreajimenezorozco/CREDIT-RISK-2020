1ed86b04f4cbe8be389e48ecadf6db9036ae30c4
Andrea Jimenez <andrea.jimenez@ieseg.fr>
2020-10-28 18:42:28 -0600

    v2

--- fintools/src/fintools/cashflow.py
+++ fintools/src/fintools/cashflow.py
@@ -8,13 +8,27 @@ class CashFlow:
         self.n = n
 
     def pv(self, r: float) -> 'CashFlow':
-        raise NotImplementedError
+        pv = self.amount / ((1 + r) ** self.n)
+        cf = CashFlow(pv, 0)
+        return cf
 
     def shift(self, n: int, r: float) -> 'CashFlow':
-        raise NotImplementedError
+        pv = self.amount / ((1 + r) ** self.n)
+        fv = pv * ((1 + r) ** n)
+        cf = CashFlow(fv, n)
+        return cf
 
     def merge(self, other: 'CashFlow', r: float, reverse: bool = False) -> 'CashFlow':
-        raise NotImplementedError
+        if reverse is False:
+            b = other.amount / ((1 + r) ** other.n)
+            b = b + self.amount
+            m = CashFlow(b, self.n)
+            return m
+        else:
+            a = self.amount * ((1 + r) ** other.n)
+            a = a + other.amount
+            m = CashFlow(a, other.n)
+            return m
 
     def to_dict(self, decimal_places: Optional[int] = 2) -> Dict:
         return {
--- src/classwork/cashflow/main.py
+++ src/classwork/cashflow/main.py
@@ -5,14 +5,16 @@ from fintools import CashFlow
 
 class Main:
 
-    def __init__(self, amount: float, t: int):
-        self.amount = amount
-        self.t = t
-
     @staticmethod
     def present_value(amount: float, rate: float, n: int):
-        return CashFlow(amount=self.amount * (1+r) ** (-self.t), t=0)
+            t = CashFlow(amount, n)
+            pv = t.pv(r=rate)
+            pv = pv.to_dict()
+            return json.dumps(pv, indent=4)
 
     @staticmethod
     def future_value(amount: float, rate: float, n: int):
-        return CashFlow(amount=self.amount * (1 + r) ** (self.t), t=t)
+            fv = amount * ((1 + rate) ** n)
+            t = CashFlow(fv, n)
+            fv = t.to_dict()
+            return json.dumps(fv, indent=4)
