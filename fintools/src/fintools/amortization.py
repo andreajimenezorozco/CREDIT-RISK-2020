49cca4ec2ca9701dc5d6b97deef77dacf0c2a5e7
Andrea Jimenez <andrea.jimenez@ieseg.fr>
2020-11-16 19:45:44 -0600

    Homework v1

--- fintools/src/fintools/amortization.py
+++ fintools/src/fintools/amortization.py
@@ -14,10 +14,39 @@ class Amortization:
 
     @property
     def annuity(self) -> float:
-        raise NotImplementedError
+        a = self.amount * self.rate / (1 - (1 + self.rate) ** (-self.n))
+        return a
 
     def get_table(self, save_file: Optional[str] = None) -> pd.DataFrame:
-        raise NotImplementedError
+        df = pd.DataFrame(index=range(0, self.n + 1), columns=["t", "B", "A", "P", "I"])
+        df["t"] = range(0, self.n + 1)
+
+        df.loc[0, "B"] = self.amount
+        df.loc[1:, "A"] = self.annuity
+
+        for j in range(1, self.n + 1):
+            df.loc[j, "I"] = df.loc[j - 1, "B"] * self.rate
+            df.loc[j, "P"] = df.loc[j, "A"] - df.loc[j, "I"]
+            df.loc[j, "B"] = df.loc[j - 1, "B"] - df.loc[j, "P"]
+        if save_file:
+            df.to_csv(save_file)
+        return df
 
     def plot(self, show: bool = False, save_file: Optional[str] = None) -> None:
-        raise NotImplementedError
+        fig, ax = plt.subplots()
+        x = self.get_table().t.values
+        bars1 = self.get_table().P.values
+        bars2 = self.get_table().I.values
+        bars1[0] = 0
+        bars2[0] = 0
+        ax.bar(x, bars1, label="P", color="blue")
+        ax.bar(x, bars2, bottom=bars1, label="I", color="orange")
+        ax.legend()
+        ax.set_title("Amortization Payment")
+        ax.set_xlabel("t")
+        ax.set_ylabel("$$$")
+        ax.grid()
+        if save_file:
+            plt.savefig(save_file)
+        if show:
+            plt.show()
--- src/homework/amortization/main.py
+++ src/homework/amortization/main.py
@@ -7,12 +7,17 @@ class Main:
 
     @staticmethod
     def annuity(amount: float, rate: float, n: int):
-        raise NotImplementedError
+        a = Amortization(amount=amount, rate=rate, n=n)
+        return round(a.annuity, 2)
 
     @staticmethod
     def table(amount: float, rate: float, n: int, save_file: Optional[str] = None):
-        raise NotImplementedError
+        a = Amortization(amount=amount, rate=rate, n=n)
+        print(a.get_table())
+        a.get_table(save_file=save_file)
 
     @staticmethod
     def plot(amount: float, rate: float, n: int, save_file: Optional[str] = None):
-        raise NotImplementedError
+        a = Amortization(amount=amount, rate=rate, n=n)
+        graph = a.plot(save_file=save_file)
+        return graph
